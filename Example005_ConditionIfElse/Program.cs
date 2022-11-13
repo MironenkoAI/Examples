129926a9c4a3bbec40eedb652734d8f172c6fc6a
Anastasiya Mironenko <nastya_911@mail.ru>
2022-11-13 20:47:42 +0300

    Написана программа приветствия с if else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿// Лекция 1, программа If Else
+Console.Write("Введите имя пользователя: ");
+string? username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же МАША!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username + "!");
+}
