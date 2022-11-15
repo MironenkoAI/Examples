cc50e8c35bec4d13d29f0670fae6fc96462cc012
Anastasiya Mironenko <nastya_911@mail.ru>
2022-11-15 22:09:13 +0300

    задачи семинара 2

---
+++ Seminar_2/Program.cs
@@ -0,0 +1,33 @@
+﻿//Задача 1: число кратно 7 и 23 одновременно?
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 7 == 0 && number % 23 == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
+
+//Задача 2: является ли одно из чисел квадратом другого?
+
+int number1 = ReadInt("Введите первое число: ");
+int number2 = ReadInt("Введите второе число: ");
+
+if (number1 * number1 == number2 || number2 * number2 == number1)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
